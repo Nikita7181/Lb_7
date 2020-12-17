57f10bc8457c808add6d58da66d1782e23562e2f
Nikita7181 <70635031+Nikita7181@users.noreply.github.com>
2020-12-17 13:47:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using std::endl;
 using std::cout;
 using std::string;
@@ -6,8 +7,8 @@ using std::cin;
 
 struct element
         {
-element* q_next;
-int value;
+element* q_next = nullptr;
+int value = -7;
 };
 
 struct Queue
@@ -18,85 +19,96 @@ struct Queue
 
 void constructor(Queue& myQueue)
 {
-    myQueue.first= nullptr;
-    myQueue.last= nullptr;
-}
-
-void destructor(Queue& myQueue)
-{
-    while (myQueue.first != nullptr)
-    {
-        element* tmp = new element;
-        tmp=myQueue.first;
-        myQueue.first=(*myQueue.first).q_next;
-        delete tmp;
-    }
-}
-
-unsigned int size(Queue& myQueue)
-{
-element* size_q = new element;
-size_q = myQueue.first;
-unsigned int counter = 0;
-while (size_q != nullptr)
-{
-    ++counter;
-    size_q = (*size_q).q_next;
-}
-    return counter;
+    myQueue.first = nullptr;
+    myQueue.last = nullptr;
 }
 
 void push(Queue& myQueue, element& l)
 {
-    if (myQueue.first == nullptr)
-    {
-        element* new_q= new element;
-        (*new_q).value = l.value;
-        myQueue.first = new_q;
-        myQueue.last = new_q;
-    } else if (myQueue.first != nullptr)
-    {
-        element* new_q = new element;
-        (*new_q).value = l.value;
-        (*new_q).q_next = new_q;
-        myQueue.last = new_q;
-
+    element* newQ_e = new element;
+    newQ_e -> value = l.value;
+    if (myQueue.first == nullptr) {
+        myQueue.first = newQ_e;
+        myQueue.last = newQ_e;
+        
+    } else if (myQueue.first != nullptr) {
+        myQueue.last -> q_next = newQ_e;
+        myQueue.last = newQ_e;
     }
+    
 }
 
 element pop(Queue& myQueue)
 {
-    int k = (*myQueue.first).value;
-    element r;
-    element* tmp = new element;
-    tmp = myQueue.first;
-    myQueue.first=(*myQueue.first).q_next;
-    delete tmp;
-    r.value = k;
-    r.q_next = myQueue.first;
-    return r;
+    int a = myQueue.first -> value;
+    element z;
+    myQueue.first = myQueue.first -> q_next;
+    z.value = a;
+    z.q_next = myQueue.first;
+    return z;
 }
+
+ int size(Queue& myQueue)
+{
+    element* c = new element;
+    c = myQueue.first;
+    unsigned int counter = 0;
+    while (c != nullptr) {
+        counter++;
+        c = c -> q_next;
+    }
+    delete c;
+    return counter;
+}
+
+void print(Queue& myQueue)
+{
+    element* c = new element;
+    c = myQueue.first;
+    while (c != nullptr) {
+        cout << c -> value << " ";
+        c = c -> q_next;
+    }
+    cout << endl;
+    delete c;
+}
+
+void del(element* myQueue)
+{
+    if (myQueue != nullptr)
+    {
+        del(myQueue -> q_next);
+        delete myQueue;
+    }
+}
+
+void destructor (Queue& myQueue)
+{
+    del(myQueue.first);
+}
+
 
 int main()
 {
-    Queue queue;
-    constructor(queue);
+    Queue myQueue;
+    constructor(myQueue);
     element element;
-    int k;
-    for (int i =0; i<5; ++i)
-    {
-        cin >> k;
-        element.value=k;
-        push(queue, element);
+    int a;
+    for (int i = 0; i < 5; ++i) {
+        std::cin >> a;
+        element.value = a;
+        push(myQueue, element);
     }
-cout <<"size:" << size(queue) << endl;
-    element = pop(queue);
-    cout << "The first element:" << element.value << endl;
-    element = pop(queue);
-    cout << "The first element:" << element.value << endl;
-    element = pop(queue);
-    cout << "The first element:" << element.value << endl;
-    cout <<"size:" << size(queue) << endl;
-    destructor(queue);
+    print(myQueue);
+    cout << "there is " << size(myQueue) << " in queue." << endl;
+    element = pop(myQueue);
+    cout << "Get the first element from queue. It is " << element.value << endl;
+    element = pop(myQueue);
+    std::cout << "Get the first element from queue. It is " << element.value << endl;
+    element = pop(myQueue);
+    cout << "Get the first element from queue. It is " << element.value << endl;
+    cout << "there is " << size(myQueue) << " in queue." << endl;
+    print(myQueue);
+    destructor(myQueue);
     return 0;
 }
