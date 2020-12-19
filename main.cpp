e7a0e97d6fa3ab3c69734654a00394296ab97a2f
Nikita7181 <70635031+Nikita7181@users.noreply.github.com>
2020-12-19 17:22:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,69 +5,68 @@ using std::cout;
 using std::string;
 using std::cin;
 
-struct element
+struct element // описание элемента очереди
 {
-    element* q_next = nullptr;
-    int value ;
+    element* q_next = nullptr;// указатель на следующий элемнт 
+    int value ;// значение ячейки
 };
 
-struct Queue
+struct Queue // описание самой очереди
 {
-    element* first;
-    element* last;
-    int count;
+    element* first;// указатель на превый элемент 
+    element* last;// указатель на последний элемент
+    int count; // счетчик элементов
 };
 
-void constructor(Queue& myQueue)
+void constructor(Queue& myQueue) // конструктор
 {
-    myQueue.first = nullptr;
-    myQueue.last = nullptr;
-    myQueue.count = 0;
+    myQueue.first = nullptr;// обнуляем адресс первого элемента
+    myQueue.last = nullptr;// обнуляем адресс последнего элемента
+    myQueue.count = 0;// обнуляем счетчик
 }
 
-
-void push(Queue& myQueue, element& l)
+void push(Queue& myQueue, element& myelement)// функция заполнения 
 {
-    element* newQ_e = new element;
-    newQ_e -> value = l.value;
-    if (myQueue.first == nullptr)
+    element* newQ_e = new element;// выделяем память под ячейки
+    newQ_e -> value = myelement.value;
+    if (myQueue.first == nullptr)// проверяем на заполненность 
     {
         myQueue.first = newQ_e;
         myQueue.last = newQ_e;
     } else
     {
-        myQueue.last -> q_next = newQ_e;
+        myQueue.last -> q_next = newQ_e; 
         myQueue.last = newQ_e;
     }
-    myQueue.count ++;
+    myQueue.count ++;// счетчик элементов
     
 }
 
-int size(Queue& myQueue)
+int size(Queue& myQueue)// функция опрделения размера очереди
 {
   return myQueue.count;
 }
 
-void print(Queue& myQueue)
+void print(Queue& myQueue)// функция печати
 {
     element* temp = myQueue.first;
     while (temp != nullptr)
     {
-        int element = temp -> value;
-        cout << element << " ";
-        temp = temp -> q_next;
+        int element = temp -> value;// вытаскиваем значение из первого элемента
+        cout << element << " ";// обычный вывод
+        temp = temp -> q_next;// переходим к следующему элементу
     }
 }
 
-void pop_front(Queue &myQueue)
+void pop_front(Queue &myQueue)// функция удаления первого элемента
 {
-    element  *temp = myQueue.first;
-     myQueue.first = myQueue.first -> q_next;
-     delete temp;
-     myQueue.count --;
+    element  *temp = myQueue.first;// временная переменная, чтобы запомнить начало списка
+     myQueue.first = myQueue.first -> q_next;//назначаем началом списка второй элемент
+     delete temp;// удаляем первыйй элемент, записанный во временную переменную
+     myQueue.count --;// уменьшаем счетчки элементов
 }
 
-void destructor (Queue& myQueue)
+void destructor (Queue& myQueue)// деструктор(работает на основе удаления преовго элемента)
 {
     while (myQueue.first != nullptr)
     {
